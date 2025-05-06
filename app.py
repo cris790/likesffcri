d49f226f4d14f59d89270207eda2bf1a4c5786b6
cris790 <150596972+cris790@users.noreply.github.com>
2025-05-06 19:12:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 def load_tokens(server_name):
     try:
         # Link direto para o JSON BR
-        url = "https://pastebin.com/raw/0mmjwjre"
+        url = "https://pastebin.com/raw/dh2FfFxC"
         
         response = requests.get(url)
         response.raise_for_status()  # Vai dar erro se a resposta não for 200 OK
